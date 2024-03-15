4be6a9902c628ac504ef8b59bb0b6330ccc4ea46
Nirmani <nirmaniwarnakula@gmail.com>
2024-03-16 00:53:13 +0530

    initial commit

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Form } from 'antd';
+
+const Register = () => {
+  return (
+    <>
+    <div className="form-container">
+      <Form> 
+        
+      </Form>
+      
+    </div>
+    </>
+  )
+}
+
+export default Register
